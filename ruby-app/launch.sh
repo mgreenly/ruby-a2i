e738d3021e72153f7f7ef27af86f9f57b12b6a24
Michael Greenly <mgreenly@gmail.com>
2025-02-01 12:06:36 -0600

    improve comments a bit

--- ruby-app/launch.sh
+++ ruby-app/launch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#
 # Ideally we'd require all *.tgz based deployables to provide a consistently
 # named script that's used as the ENTRYPOINT. In this example I assume this
 # file is that cript. The only requirements of this script is that it runs
@@ -7,8 +8,15 @@
 # exit code is returned, beyond that there is no requirements on how it starts
 # and runs the app. This makes this part of the process completely language
 # agnostic.
+#
+# If done correctly you can use this script to run the app on your laptop as
+# well as it being the entry point when the app is deployed in production.
+#
+# This script is utltimately created and controlled by the developers.  So this
+# is just an example of how we'd do it for our ruby apps.
+# 
 
-# exit if there's an error and show the commands as they run
+# trace all commands and exit if there are any errors
 set -ex
 
 # provide some helpful debug info about the system
